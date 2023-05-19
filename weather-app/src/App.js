7d354f491df78a9ca65e7102ff6ffe62f96f8d49
Luana Alves <luanaalvessoares_@outlook.com>
2023-05-19 12:16:07 -0300

    feat: weather api consumption

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [weatherData, setWeatherData] = useState(null);
+  const [address, setAddress] = useState('');
+
+  const fetchWeatherData = async (latitude, longitude) => {
+    const API_KEY = 'b300e5c756f3d0baeaffb67a5f994839';
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`
+    );
+    setWeatherData(response.data);
+  };
+
+
+
+  
 }
 
 export default App;
